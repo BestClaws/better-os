e4606f615ab214292f012843a4c0a6e2e1f36fec
Boby <liya@sliya.in>
2025-05-11 15:32:38 +0530

    perfect

---
+++ src/bin/main.rs
@@ -0,0 +1,97 @@
+#![no_std]
+#![no_main]
+
+use bt_hci::controller::ExternalController;
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_futures::join::join;
+use embassy_time::{Duration, Timer};
+use esp_hal::clock::CpuClock;
+use esp_hal::timer::systimer::SystemTimer;
+use esp_hal::timer::timg::TimerGroup;
+use esp_wifi::ble::controller::BleConnector;
+use trouble_host::{prelude::{AdStructure, Advertisement, AdvertisementParameters, DefaultPacketPool, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE}, Address, Host, HostResources};
+use panic_rtt_target as _;
+extern crate alloc;
+
+#[esp_hal_embassy::main]
+async fn main(spawner: Spawner) {
+    // generator version: 0.3.1
+    rtt_target::rtt_init_defmt!();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+
+    let timer0 = SystemTimer::new(peripherals.SYSTIMER);
+    esp_hal_embassy::init(timer0.alarm0);
+
+    info!("Embassy initialized!");
+
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let init = esp_wifi::init(
+        timer1.timer0,
+        esp_hal::rng::Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+
+    let connector = BleConnector::new(&init, peripherals.BT);
+    let controller: ExternalController<_, 20> = ExternalController::new(connector);
+
+    // TODO: Spawn some tasks
+    let _ = spawner;
+
+    let address: Address = Address::random([0xff, 0x8f, 0x1a, 0x05, 0xe4, 0xff]);
+    info!("Our address");
+
+    let mut resources: HostResources<DefaultPacketPool, 0, 0> = HostResources::new();
+    let stack = trouble_host::new(controller, &mut resources).set_random_address(address);
+    let Host {
+        mut peripheral,
+        mut runner,
+        ..
+    } = stack.build();
+
+    let mut adv_data = [0; 31];
+    let len = AdStructure::encode_slice(
+        &[
+            AdStructure::CompleteLocalName(b"Trouble Advert"),
+            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+        ],
+        &mut adv_data[..],
+    )
+    .unwrap();
+
+    info!("Starting advertising");
+    let _ = join(runner.run(), async {
+        loop {
+            let mut params = AdvertisementParameters::default();
+            params.interval_min = Duration::from_millis(100);
+            params.interval_max = Duration::from_millis(100);
+            let _advertiser = peripheral
+                .advertise(
+                    &params,
+                    Advertisement::NonconnectableScannableUndirected {
+                        adv_data: &adv_data[..len],
+                        scan_data: &[],
+                    },
+                )
+                .await
+                .unwrap();
+            loop {
+                info!("Still running");
+                Timer::after(Duration::from_secs(60)).await;
+            }
+        }
+    })
+    .await;
+
+    loop {
+        info!("Hello world!");
+        Timer::after(Duration::from_secs(1)).await;
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
+}
