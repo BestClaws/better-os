240f25b8efb539f657cf0381512c3cc8e1a8f33c
Boby <liya@sliya.in>
2025-07-06 11:52:12 +0530

    best encoder settings

--- src/system/hal/encoder.rs
+++ src/system/hal/encoder.rs
@@ -40,22 +40,26 @@ pub trait AsyncEncoder {
 #[async_trait(?Send)]
 impl<P: InputPin + Wait> AsyncEncoder for EncoderDriver<P> {
     async fn next(&mut self) -> Result<EncoderState, EncoderError> {
-        self.a_pin.wait_for_falling_edge().await;
+
+        loop {
+            self.a_pin.wait_for_falling_edge().await;
 
 
 
-        // 2) debounce
-        Timer::after(Duration::from_millis(50)).await;
+            // 2) debounce
+            Timer::after(Duration::from_millis(2)).await;
 
-        // 3) sample
-        let a = self.a_pin.is_high().map_err(|_| EncoderError::PinError)?;
-        let b = self.b_pin.is_high().map_err(|_| EncoderError::PinError)?;
+            // 3) sample
+            let a = self.a_pin.is_high().map_err(|_| EncoderError::PinError)?;
+            let b = self.b_pin.is_high().map_err(|_| EncoderError::PinError)?;
 
-        // 4) decode
-        match (a, b) {
-            (false, true)  => Ok(EncoderState::Ccw),
-            (false, false) => Ok(EncoderState::Cw),
-            _              => Err(EncoderError::InvalidState),
+            // 4) decode
+            return match (a, b) {
+                (false, true)  => Ok(EncoderState::Ccw),
+                (false, false) => Ok(EncoderState::Cw),
+                _              => continue,
+            }
         }
+
     }
 }
