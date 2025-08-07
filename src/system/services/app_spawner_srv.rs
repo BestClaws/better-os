cae09a57efe58d47a906ec89bb8f84711ba2edbf
boby <me@sliya.in>
2025-08-07 05:46:48 +0530

    refactor app spawner for simpler app spawns

--- src/system/services/app_spawner_srv.rs
+++ src/system/services/app_spawner_srv.rs
@@ -15,33 +15,26 @@ pub async fn app_spawner_service(
     compositor: &'static Mutex<CriticalSectionRawMutex, UICompositor>,
     spawner: Spawner,
 ) {
+
+    let ctx = create_context(spawner, compositor, 0, "arrow", 0).await;
+    spawner.spawn(arrow_app(ctx)).unwrap();
+    let ctx = create_context(spawner, compositor, 1, "ble", 1).await;
+    spawner.spawn(ble_app(ctx)).unwrap();
+    let ctx = create_context(spawner, compositor, 2, "notification", 2).await;
+    spawner.spawn(notifications_app(ctx)).unwrap();
+    let ctx = create_context(spawner, compositor, 3, "dummy", 3).await;
+    spawner.spawn(dummy_app(ctx)).unwrap();
+
+
+}
+
+
+pub async fn create_context(spawner: Spawner, compositor: &'static Mutex<CriticalSectionRawMutex, UICompositor>, id: usize, name: &'static str, app: i32) -> AppContext {
     let mut comp = compositor.lock().await;
 
     let whandle = comp
-        .new_window(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 0)
-        .await
-        .expect("Failed to allocate battery window");
-    let ctx = AppContext::new(whandle, 0, "hello", compositor);
-    spawner.spawn(arrow_app(ctx)).unwrap();
-
-    let whandle = comp
-        .new_window(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 1)
-        .await
-        .expect("Failed to allocate battery window");
-    let ctx = AppContext::new(whandle, 1, "ble", compositor);
-    spawner.spawn(ble_app(ctx)).unwrap();
-
-    let whandle = comp
-        .new_window(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 2)
-        .await
-        .expect("Failed to allocate battery window");
-    let ctx = AppContext::new(whandle, 2, "notification", compositor);
-    spawner.spawn(notifications_app(ctx)).unwrap();
-
-    let whandle = comp
-        .new_window(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, 3)
-        .await
-        .expect("Failed to allocate battery window");
-    let ctx = AppContext::new(whandle, 3, "dummy", compositor);
-    spawner.spawn(dummy_app(ctx)).unwrap();
+    .new_window(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT, id)
+    .await
+    .expect("Failed to allocate app window");
+     AppContext::new(whandle, id, name, compositor)
 }
